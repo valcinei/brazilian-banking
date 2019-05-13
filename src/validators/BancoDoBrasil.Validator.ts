4e692a2af7535e76649010458c0dbc8cb9ce8a43
Valcinei Silva <valcinei.silva@fpf.br>
2019-05-13 14:56:25 -0400

    reimplement

--- src/validators/BancoDoBrasil.Validator.ts
+++ src/validators/BancoDoBrasil.Validator.ts
@@ -18,16 +18,13 @@ export class BancoDoBrasilValidator extends CommonValidator implements CommonVal
 
     public validateAccount(accountNumber: string, accountDigit: string): boolean {
         accountNumber = this.addLeftZero(accountNumber, 8)
-        let splitedNumbers = accountNumber.split("")
-        let calculatedDigit = this.getCalculatedSequency(splitedNumbers, 9);
+        let calculatedDigit = this.getCalculatedSequency(accountNumber, 9);
         return accountDigit.toUpperCase() === calculatedDigit;
     }
 
     public valdateAgency(agencyNumber: string, agencyDigit: string): boolean {
-
-        agencyNumber = this.addLeftZero(agencyNumber,4)
-        let splitedNumbers = agencyNumber.split("");
-        let calculatedDigit = this.getCalculatedSequency(splitedNumbers, 5);
+        agencyNumber = this.addLeftZero(agencyNumber, 4)
+        let calculatedDigit = this.getCalculatedSequency(agencyNumber, 5);
         let validAgencyDigit:boolean = false;
 
         if(agencyDigit.length){
@@ -41,8 +38,9 @@ export class BancoDoBrasilValidator extends CommonValidator implements CommonVal
 
     }
 
-    private getCalculatedSequency(splitedNumbers: Array<any>, subtractValue: number) {
+    private getCalculatedSequency(sequencyNumber: string, subtractValue: number) {
         let sumSeq = 0
+        let splitedNumbers = sequencyNumber.split("");
         splitedNumbers.forEach((item: any, index: number) => {
             let seq = subtractValue - index
             sumSeq += (parseInt(item) * seq)
