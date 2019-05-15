7a09eb636c4c6fefd97f73513a91ac50d0b92844
Valcinei Silva <valcinei.silva@fpf.br>
2019-05-15 15:38:29 -0400

    update exemple

--- exemples/BB.js
+++ exemples/BB.js
@@ -2,7 +2,17 @@ const validator = require('../dist');
 const BankingInfo = require('../dist').BankingInfo;
 
 let bbValidator = new validator.BancoDoBrasil();
-let ankInfo = new BankingInfo('1158253','7','3030')
 
-console.log(bbData)
-console.log(bbValidator.validate( { accountNumber: '1158253', accountDigit: '7',  agencyNumber: '3030', agencyDigit: '' }))
+let bankingInfoData = new BankingInfo('1158253','7','3030');
+
+console.log(bbValidator.validate(bankingInfoData));
+
+/* OR */
+
+let bankingData =  {
+accountNumber:'1158253',
+accountDigit:'7',
+agencyNumber:'3030'
+}
+
+console.log(bbValidator.validate( bankingData));
